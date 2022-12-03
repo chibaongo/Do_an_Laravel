e648a1c69547c3560cdf84175556bcbbd12461f5
chibaongo <baokun6702@gmail.com>
2022-12-03 12:36:40 +0700

    chinh search

--- admin-kiemtthu/resources/views/pages/search.blade.php
+++ admin-kiemtthu/resources/views/pages/search.blade.php
@@ -22,20 +22,20 @@
                                         </div>
                                         <div class="product-overlay">
                                             <div class="overlay-content">
-                                                <h2>{{ $value->Price ?? '' }}</h2>
-                                                <p>{{ $value->Name ?? '' }}</p>
+                                                {{-- <h2>{{ $value->Price ?? '' }}</h2>
+                                                <p>{{ $value->Name ?? '' }}</p> --}}
                                                 <a href="{{ url("product/$value->id ") }}"
                                                     class="btn btn-default add-to-cart"><i
                                                         class="fa fa-shopping-cart"></i>View Detail</a>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="choose">
+                                    {{-- <div class="choose">
                                         <ul class="nav nav-pills nav-justified">
                                             <li><a href="#"><i class="fa fa-plus-square"></i>Add to wishlist</a></li>
                                             <li><a href="#"><i class="fa fa-plus-square"></i>Add to compare</a></li>
                                         </ul>
-                                    </div>
+                                    </div> --}}
                                 </div>
                             </div>
                         @endforeach
